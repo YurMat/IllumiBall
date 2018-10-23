141ed07609635bc114d4559d7f7089a4e111494e
YurMat <yurimatsu28@gmail.com>
2018-10-23 15:00:17 +0900

    create Hole script and FallInChecker script

---
+++ Assets/Scripts/FallInChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallInChecker : MonoBehaviour {
+
+    public Hole red;
+    public Hole green;
+    public Hole blue;
+
+    private void OnGUI()
+    {
+        string label = " ";
+
+        if (red.InFallIn() && green.InFallIn() && blue.InFallIn())
+        {
+            label = "Fall in hole";
+        }
+        GUI.Label(new Rect(0, 0, 100, 30), label);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -2,10 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Hole : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    bool fallIn;
+
+    public string activeTag;
+
+    public bool InFallIn()
+    {
+        return fallIn;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == activeTag)
+        {
+            fallIn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == activeTag)
+        {
+            fallIn = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody r = other.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 direction = transform.position - other.gameObject.transform.position;
+        direction.Normalize();
+
+
+        if (other.gameObject.tag == activeTag)
+        {
+
+            r.velocity *= 0.9f;
+            r.AddForce(direction * r.mass * 20.0f);
+        }
+        else
+        {
+            r.AddForce(-direction * r.mass * 80.0f);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
